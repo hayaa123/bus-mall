707d3099ac60dbe3f8f2e1f33bdf9db96cf7f404
hayaa1234 <hayalawansah@gmail.com>
2021-08-09 21:45:21 +0300

    improve css

--- js/app.js
+++ js/app.js
@@ -129,12 +129,12 @@ function bar_chart(){
         label: '# of Votes',
         data: char_data,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          '#63BFF0',
+          '#06E5DC',
+          '#67F1B8',
+          '#B0F88F',
+          '#F9F871',
+          '#19D3F0'
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
